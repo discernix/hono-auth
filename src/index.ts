a1e9d1dafe79a68b0c06d62fa226bb43bf5d008d
Bintang <itzbintangyosua@gmail.com>
2024-11-09 08:37:06 +0700

    from hono to vercel

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import { Hono } from "hono";
 import { serveStatic } from "@hono/node-server/serve-static";
 import home from "./pages";
 import auth from "./pages/auth";
-import https from "https";
 import fs from "fs";
+import { handle } from "hono/vercel";
 
-const certOptions = {
-  key: fs.readFileSync("localhost-key.pem"),
-  cert: fs.readFileSync("localhost.pem"),
-};
+export const runtime = "edge";
 
 const app = new Hono();
 
@@ -18,10 +15,15 @@ app.use("/public/*", serveStatic({ root: "./" }));
 app.route("/", home);
 app.route("/auth", auth);
 
-const port = 3000;
-console.log(`Server is running on port ${port}: http://localhost:3000/`);
+// const port = 3000;
+// console.log(`Server is running on port ${port}: http://localhost:3000/`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+
+// serve({
+//   fetch: app.fetch,
+//   port,
+// });
